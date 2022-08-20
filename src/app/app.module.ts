ea11ca209a98600f51bea60b2e1a059946bb33eb
mohammedDbs <mohammed.dbs.660@gmail.com>
2022-08-20 10:37:40 +0200

    reactive forms p1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms'; 
+import {MatSelectModule} from '@angular/material/select';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -45,6 +48,9 @@ import { FormsModule } from '@angular/forms';
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule,
     FormsModule
    
   ],
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { ContactType } from './../shared/feddback';
 
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import {Feedback } from '../shared/feddback';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
+feedbackForm : FormGroup;
+feedback : Feedback;
+contactType : ContactType;
+  constructor(private fb : FormBuilder) {
+    this.createForm();
+   }
 
   ngOnInit(): void {
   }
+
+  createForm(){
+    this.feedbackForm = this.fb.group({
+      firstname : '',
+      lastname : '',
+      telnum : 0,
+      email : '',
+      agree : false,
+      Contacttype : 'None',
+      message : ''
+    });
+
+  }
+  
 
 }
